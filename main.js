799fd2ba0dddf58eb0c71d6a1bd2f82cabb5ac65
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-02 23:05:48 -0800

    Allow sorting in reverse order

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ $(document).ready(() => {
 
   // initialize page
   let sortBy = 0;
+  let reverseSort = false;
   let list = localStorage.list ? JSON.parse(localStorage.list) : [];
   printList();
   $('#name').focus();
-
 
   $('#clear').click(() => { updateList([]); });
   $('#add').click(() => { addContact(); });
@@ -18,9 +18,16 @@ $(document).ready(() => {
   });
 
   function changeSortBy() {
-    sortBy = $(this).prevAll().length;
-    $('.header:nth-of-type(' + (sortBy + 1) + ') h4').append( $('#caret') );
-    
+    let newSortBy = $(this).prevAll().length;
+
+    if (sortBy === newSortBy) {
+      reverseSort = !reverseSort;
+      $('#caret').toggleClass('fa-caret-up').toggleClass('fa-caret-down');
+    } else {
+      sortBy = newSortBy;
+      $('.header:nth-of-type(' + (sortBy + 1) + ') > *').append( $('#caret') );
+    }
+
     printList();
   }
 
@@ -30,9 +37,10 @@ $(document).ready(() => {
     let phone = $('#phone').val();
     let twitter = $('#twitter').val();
     updateList( list.concat([[name, email, phone, twitter]]) );
+    $('.permanent input').val('');
     $('#name').focus();
 
-    $('#phone').val( Math.floor(Math.random() * 9000000000) + 1000000000 ); // DEBUG
+    // $('#phone').val( Math.floor(Math.random() * 9000000000) + 1000000000 ); // DEBUG
   }
 
   function removeContact() {
@@ -45,13 +53,13 @@ $(document).ready(() => {
     $('tr').not('.permanent').remove();
 
     list.sort(function(a, b) {
-      return a[sortBy] > b[sortBy];
+      return reverseSort ? a[sortBy] < b[sortBy] : a[sortBy] > b[sortBy];
     });
 
     let newTableRows = [];
     list.forEach((person, i) => {
       // create table row entry representing that person
-      let $removeButton = $('<button>').text('X').click(removeContact);
+      let $removeButton = $('<button>').html('<i class="fa fa-trash-o"></i>').click(removeContact);
       let $entry = $('<tr>').append( $('<td>').text(person[0]) )
                             .append( $('<td>').text(person[1]) )
                             .append( $('<td>').text(person[2]) )
@@ -73,6 +81,7 @@ $(document).ready(() => {
     let indexToUpdate = $(this).closest('tr').attr('index');
     let fieldToUpdate = $(this).prevAll().length;
     let newValue = prompt('New Value:', $(this).text());
+
     if (newValue) {
       list[indexToUpdate][fieldToUpdate] = newValue;
       updateList(list);
